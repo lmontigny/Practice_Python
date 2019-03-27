a81957d591ff137e4ad9c1e4ed7cff2c2db25849
Laurent Montigny <lmontigny@users.noreply.github.com>
2019-03-27 11:01:29 +0100

    Update read_files.py

--- read_files.py
+++ read_files.py
@@ -1,4 +1,6 @@
 # Different function
+# More example here: https://stackoverflow.com/questions/29454365/what-does-sys-stdin-read
+
 write	filevar.write(astring)	
 read(n)	filevar.read()
 readline(n)	filevar.readline()	
